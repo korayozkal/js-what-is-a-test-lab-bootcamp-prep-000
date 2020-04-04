24f30c42892734351c74f9ea892e5afe4cfda7ca
Koray Ozkal <koryozkal@gmail.com>
2020-04-04 05:01:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ index.js
 Full Screen
 // write your code below!
 var name = "Susan"
-var height = 40
+var height = "40"
 var message = `is ${height} inches tall`
 
 module.exports = {name, height, message}
